0c69b67871f07df15236034372e4af987b47305e
Nahuel <nahum8798@users.noreply.github.com>
2024-04-17 21:20:02 -0300

    Se mejoro la logica de inicio de sesion Se creo el documento pagina_principal.html

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 /* Obtención de elementos del DOM */
 
 const form = document.getElementById('login-form');
-const btn = document.getElementById('login-btn')
 const username = document.getElementById('username');
 const password = document.getElementById('password');
+const userWrong = document.getElementById('hidden-username');
+const passWrong = document.getElementById('hidden-password');
+
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();                 /* Prevenir el envío del formulario */
+    e.preventDefault(); /* Prevenir el envío del formulario */
 
     // Validar datos ingresados
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
 
-    if (username.trim() === '') {
-
-        alert('Por favor, ingrese su nombre de usuario,');
+    if (username === '') {
+        alert('Por favor, ingrese su nombre de usuario.');
         return;
     }
 
-    if (password.trim() === '') {
+    if (password === '') {
         alert('Por favor, ingrese su contraseña.');
         return;
     }
 
     // Enviar datos del inicio de sesión a una función para su procesamiento
+    login(usernameValue, passwordValue);
+});
 
-    const login = (username,password) =>{
+// Función de inicio de sesión
+const login = (username, password) => {
+    // Aquí puedes agregar la lógica para verificar las credenciales del usuario
+    // Por ejemplo, consultar una base de datos para comprobar si el usuario y la contraseña son válidos
+    // Si las credenciales son válidas, puedes redirigir al usuario a la página principal de la aplicación
 
-         // Aquí puedes agregar la lógica para verificar las credenciales del usuario
-         // Por ejemplo, consultar una base de datos para comprobar si el usuario y la contraseña son válidos
-        // Si las credenciales son válidas, puedes redirigir al usuario a la página principal de la aplicación
-        
-
-        if (username === 'usuario' && password === '123456') {
-            window.location.href = '';
+    if (username === 'usuario' && password === '123456') {
+        window.location.href = 'pagina_principal.html';
+    } else {
+        if (username !== 'usuario') {
+            userWrong.innerText = "Nombre de usuario incorrecto."
         }
-
-    };
-
-})
-
+        if (password !== '123456') {
+            passWrong.innerText = "Contraseña incorrecta."
+        }
+    }
+};
